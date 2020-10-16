0984fab3aab07b54590a6f38d6ecdc93c642f892
Lallo Vigil <lallo.vigil@gmail.com>
2020-10-15 22:50:14 -0500

    More projects added

--- src/mock/data.js
+++ src/mock/data.js
@@ -30,7 +30,7 @@ export const projectsData = [
     id: uuidv1(),
     img: 'project.jpg',
     title: 'Interactive Campus Map',
-    info: 'An interactive campus map that I helped create using Leaflet, jQuery and Bootstrap.',
+    info: 'An interactive campus map that I helped create using Leaflet, jQuery, and Bootstrap.',
     info2: '',
     url: 'https://www.csupueblo.edu/about/campus/interactive-map.html',
     repo: 'https://github.com/lalov1/campus-map', // if no repo, the button will not show up
@@ -39,10 +39,12 @@ export const projectsData = [
     id: uuidv1(),
     img: 'project.jpg',
     title: 'Hits the Spot',
-    info: '',
+    info:
+      'A project we built for the GoCodeColorado competition, which we were a runner up. We did the project with Leaflet, jQuery, and Bootstrap.',
     info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    url:
+      'https://gazette.com/business/colordo-springs-teams-developing-apps-for-restaurants-water-rights-advance/article_0a1f6e48-8726-5e67-a3c1-3e0cf1b6b4b8.html',
+    repo: 'https://github.com/GoCodeColorado/CacheMoney', // if no repo, the button will not show up
   },
   {
     id: uuidv1(),
@@ -50,8 +52,27 @@ export const projectsData = [
     title: 'Abodio',
     info: '',
     info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    url:
+      'https://www.sos.state.co.us/pubs/newsRoom/pressReleases/2018/PR20180608GoCodeWinners.html',
+    repo: 'https://github.com/GoCodeColorado/Abodio', // if no repo, the button will not show up
+  },
+  {
+    id: uuidv1(),
+    img: 'project.jpg',
+    title: 'University of the Incarnate Word',
+    info: '',
+    info2: '',
+    url: 'https://www.uiw.edu/',
+    repo: '', // if no repo, the button will not show up
+  },
+  {
+    id: uuidv1(),
+    img: 'project.jpg',
+    title: 'Colorado State University-Pueblo',
+    info: '',
+    info2: '',
+    url: 'https://www.csupueblo.edu/',
+    repo: '', // if no repo, the button will not show up
   },
 ];
 
